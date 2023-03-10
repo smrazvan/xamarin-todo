438ffabf822573ab54ffd95debe5c444ec108d6f
smrazvan <serbrazvann@gmail.com>
2023-03-10 15:47:12 +0200

    changed icon

--- routedapp/routedapp.Android/MainActivity.cs
+++ routedapp/routedapp.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace routedapp.Droid
 {
-    [Activity(Label = "routedapp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "OrganizR", Icon = "@drawable/kitty", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
